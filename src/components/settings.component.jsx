ea8a1215e2dd1aa6d2a0cb5cb5224dee826f2a99
Workspace <gobind.jha@yahoo.com>
2020-05-06 08:16:48 +0530

    state addition

--- src/components/settings.component.jsx
+++ src/components/settings.component.jsx
@@ -3,32 +3,66 @@ import './settings.styles.scss';
 
 class Settings extends React.Component{
 
+    constructor(){
+        super();
+
+        this.state = {
+            name:'',
+            dob:'',
+            profession:'',
+            designation:''
+        }
+    }
+
+    handleSubmit = event =>{
+        const {name, dob, profession,designation}=this.state;
+        event.preventDefault();
+
+        this.setState({
+            name:'',
+            dob:'',
+            profession:'',
+            designation:''
+        })
+
+        console.log({name,dob,profession,designation});
+    }
+
+
+    handleChange = event =>{
+        const {value,name} = event.target;
+        this.setState({[name]:value});
+    }
+
+
+
 
     render(){
+        const {name, dob, profession, designation} = this.state; 
         return(
             
             <div className="settings">
                 <h2>Profile Info</h2>
-                <form className="setting__form">
+                <form className="setting__form" onSubmit={this.handleSubmit} >
                 <div className="settings__fields">
                     <label>Name</label>
-                    <input type='text' placeholder='Name' maxLength='30' required  pattern="[A-Za-z]"/>
+                    <input  onChange={this.handleChange} name='name' value={name} type='text' placeholder='Name' maxLength='30'  pattern="[A-Za-z]{0,}" required/>
                 </div>
                 <div className="settings__fields">
                     <label>DOB</label>
-                    <input type='date' placeholder='DOB' required/>
+                    <input  onChange={this.handleChange} name='dob' value={dob} type='date' placeholder='DOB' required/>
                 </div>
                 <div className="settings__fields">
                     <label>Profession</label>
-                    <input type='text' placeholder='Profession' required pattern="[A-Za-z]"/>
+                    <input  onChange={this.handleChange}  name='profession' value={profession} type='text' placeholder='Profession'  pattern="[A-Za-z]{0,}" required/>
                 </div>
                 <div className="settings__fields">
                     <label>Designation</label>
-                    <input type='text' placeholder='Designation'required pattern="[A-Za-z]"/>
+                    <input  onChange={this.handleChange} name='designation' value={designation} type='text' placeholder='Designation' pattern="[A-Za-z]{0,}" required/>
                 </div>
                 <div className='settings__button-box'>
-                    <button className='settings__button --cancel'>Cancel</button>
-                    <button className='settings__button --submit'>Submit</button>
+                    <button type='reset' className='settings__button --cancel'>Cancel</button>
+                    <button type='submit' className='settings__button --submit'>Submit</button>
                 </div>  
 
                 </form>  
