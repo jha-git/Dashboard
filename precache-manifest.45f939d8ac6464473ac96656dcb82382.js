e5f7c1fdae492d120e3797dfde55ebef49414366
jha-git <gobind.jha@yahoo.com>
2020-08-27 14:06:54 +0530

    Updates

---
+++ precache-manifest.45f939d8ac6464473ac96656dcb82382.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2c25897f8b61801d8fd1e88ccd6378",
+    "url": "/Dashboard/index.html"
+  },
+  {
+    "revision": "0dab9af9be6a42b9d35d",
+    "url": "/Dashboard/static/css/main.ba8ffe47.chunk.css"
+  },
+  {
+    "revision": "70259ee4fd1305d25bb4",
+    "url": "/Dashboard/static/js/2.e49c9df2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Dashboard/static/js/2.e49c9df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dab9af9be6a42b9d35d",
+    "url": "/Dashboard/static/js/main.95ad1b5a.chunk.js"
+  },
+  {
+    "revision": "8a5f27a19afa08c82cc4",
+    "url": "/Dashboard/static/js/runtime-main.b1cf1eed.js"
+  },
+  {
+    "revision": "516a08d99c90f5a64f77a3f425883ead",
+    "url": "/Dashboard/static/media/user-1.516a08d9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Jupiter-Assignment/precache-manifest.b23bd18c3090edafbaeb4575e62912df.js"
+  "/Dashboard/precache-manifest.45f939d8ac6464473ac96656dcb82382.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jupiter-Assignment/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
